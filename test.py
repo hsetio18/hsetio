60f68d04b5815e59b585453ef6345796fa81b2c7
hsetio18 <h_setio@yahoo.com>
2025-04-12 08:12:25 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,14 @@
-def funct1():
-  df=101
-  print("funct1",df)
-  return df
-def funct2():
+df=100
+df main():
+  def funct1():
+    df=101
+    print("funct1",df)
+    return df
+  def funct2():
     df=102
     print("in funct2",df)
     return df
+  df=funct2()
+  print df
+if __main__=="__main__":
+  main()
