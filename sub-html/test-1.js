4afce63614461911883e086cdc2b978c58c42765
hsetio18 <h_setio@yahoo.com>
2025-06-13 16:02:15 +0700

    Update test-1.js

--- sub-html/test-1.js
+++ sub-html/test-1.js
@@ -97,7 +97,7 @@ function checkAnswers(problems) {
       ${correct ? "✅ Correct!" : "❌ Incorrect"}
       <br>Correct Answer: ${computed}
       <br>${q.explanation || ""}
-      <br><small><code>${q.__expression}</code></small>
+      // <br><small><code>${q.__expression}</code></small>
     `;
   });
 }
