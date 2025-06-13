460e7a0a7686047939c91bb4955831e0fb709d9b
hsetio18 <h_setio@yahoo.com>
2025-06-13 11:15:14 +0700

    Update test-1.js

--- sub-html/test-1.js
+++ sub-html/test-1.js
@@ -4,9 +4,9 @@ const title = params.get('title') || '....';
 
 
 // Update the page title (e.g., in <h1 id="quiz-title">...</h1>)
-document.addEventListener("DOMContentLoaded", () => {
+// document.addEventListener("DOMContentLoaded", () => {
   const titleElement = document.getElementById("quiz-title");
   if (titleElement) {
     titleElement.textContent = decodeURIComponent(title);
   }
-});
+//});
