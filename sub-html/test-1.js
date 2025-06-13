e25b02b0e00738e24cf0f87a6651f7f42202fb2c
hsetio18 <h_setio@yahoo.com>
2025-06-13 11:45:56 +0700

    Update test-1.js

--- sub-html/test-1.js
+++ sub-html/test-1.js
@@ -1,13 +1,9 @@
 const params = new URLSearchParams(window.location.search);
 const file = params.get('file') || 'questions.json';
-const title = params.get('title') || '....';
-        window.alert("in test-1.js");
-
-
-// Update the page title (e.g., in <h1 id="quiz-title">...</h1>)
-// document.addEventListener("DOMContentLoaded", () => {
-  const titleElement = document.getElementById("quiz-title");
-  if (titleElement) {
+const title = params.get('title') || '....'
+// Update the page title not working
+const titleElement = document.getElementById("quiz-title");
+if (titleElement) {
     titleElement.textContent = decodeURIComponent(title);
-  }
-//});
+}
+
