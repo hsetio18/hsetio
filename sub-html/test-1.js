f331c3cec2b314b679d506b8dca917e5fbb8d46c
hsetio18 <h_setio@yahoo.com>
2025-06-13 11:12:05 +0700

    Create test-1.js

---
+++ sub-html/test-1.js
@@ -0,0 +1,12 @@
+const params = new URLSearchParams(window.location.search);
+const file = params.get('file') || 'questions.json';
+const title = params.get('title') || '....';
+
+
+// Update the page title (e.g., in <h1 id="quiz-title">...</h1>)
+document.addEventListener("DOMContentLoaded", () => {
+  const titleElement = document.getElementById("quiz-title");
+  if (titleElement) {
+    titleElement.textContent = decodeURIComponent(title);
+  }
+});
