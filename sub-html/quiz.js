8814786c13c0f31e575b76bfa2fa97b398628268
hsetio18 <h_setio@yahoo.com>
2025-07-06 16:29:51 +0700

    Update quiz.js

--- sub-html/quiz.js
+++ sub-html/quiz.js
@@ -69,18 +69,20 @@ function showQuestion(randomizeVars = true) {
   box.innerHTML = "";
   q.__values = generateValues(q.variables || {}, randomizeVars);
   let text = q.problem;
-  alert("a:"+text);
+  // alert("a:"+text);
   for (const [k, v] of Object.entries(q.__values)) {
     const display = v < 0 ? `− ${Math.abs(v)}` : `${v}`;
     text = text.replaceAll(`{${k}}`, display);
   }
-  alert("b"+text);
+  // alert("b"+text);
   box.innerHTML += `<p><strong>Q${current + 1}:</strong> ${text}</p>`;
 
   if (q.answer_type === "number") {
-    alert("c. formula="+q.formula);
+    // alert("c. formula="+q.formula);
     q.__expr = substitute(q.formula, q.__values);
-    box.innerHTML += `<input type="number" id="ans" step="any">`;
+    const unit = q.unit ? ` ${q.unit}` : "";
+    box.innerHTML += `<label>The answer = <input type="number" id="ans" step="any">${unit}</label>`;
+    // box.innerHTML += `<input type="number" id="ans" step="any">`;
   } else if (q.answer_type === "mc") {
     let choices = [];
     let correct = q.correct_choice;
