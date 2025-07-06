4042e155505b37b88bfb5f58321a4c5d4c2ed87b
hsetio18 <h_setio@yahoo.com>
2025-07-06 19:18:03 +0700

    Update quiz.js

--- sub-html/quiz.js
+++ sub-html/quiz.js
@@ -80,7 +80,7 @@ function showQuestion(randomizeVars = true) {
   if (q.answer_type === "number") {
     // alert("c. formula="+q.formula);
     q.__expr = substitute(q.formula, q.__values);
-    const decimal = q.decimals ? ${q.decimal}+" digits of decimal" : "";
+    const decimal = q.decimals ? ${q.decimals}+" digits of decimal" : "";
     const unit = q.unit ? ` ${q.unit}` : "";
     // box.innerHTML += `<label>The answer = <input type="number" id="ans" step="any">${unit}</label>`;
     box.innerHTML += `<label>The answer = <input type="number" id="ans" step="any">${unit}+${decimal}</label>`;
